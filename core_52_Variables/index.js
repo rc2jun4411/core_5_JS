5cefc6bd82b9f383e8d37d6e15ade6148c34c843
Junichi Kimura <77219941+rc2jun4411@users.noreply.github.com>
2026-09-15 13:12:31 +0900

    3rd commit

---
+++ core_52_Variables/index.js
@@ -0,0 +1,26 @@
+/* C:\Users\rc2ju\OneDrive\Documents\core_5_JS\core_52_Variables\index.js */
+
+/* ex-50.5,  */
+const buttonA = document.querySelector("#button_A");
+const headingA = document.querySelector("#heading_A");
+
+let count = 1;
+
+/* button Press meをclickすると一回実行される。
+   countが+1される。*/
+
+// mod. Gemini A1
+/* JS: wrap the count inside a <span> element */
+buttonA.onclick = () => {
+  buttonA.textContent = "Try again!";
+  headingA.innerHTML = `<span class="count-red">${count}
+                        </span> clicks so far`;
+  count += 1;
+};   
+
+// old
+// buttonA.onclick = () => {
+//   buttonA.textContent = "Try again!";
+//   headingA.textContent = `${count} clicks so far`;
+//   count += 1;
+// };
